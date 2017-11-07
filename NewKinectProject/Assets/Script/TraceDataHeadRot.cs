29901fb8046cd93d48d9ee89f6235fd0e145fed4
Ikiriss <32384371+Ikiriss@users.noreply.github.com>
2017-11-07 10:20:44 +0100

    debut TP

---
+++ NewKinectProject/Assets/Script/TraceDataHeadRot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+public class TraceDataHeadRot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        File.AppendAllText("DataHeadRot.txt", "" + transform.eulerAngles + "\r\n");
+    }
+}
